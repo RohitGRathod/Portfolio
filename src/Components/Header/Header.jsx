61873cc4e4a1ed96513072d395b4310cdb23ad89
Rohit Rathod <rohit.gr.rathod@gmail.com>
2025-08-02 18:17:45 +0530

    Your message

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -37,7 +37,7 @@ function Header() {
         navItem.map((item)=>
             <li key={item.title}>
             <button 
-            className='bg-gray-600 hover:bg-black/40 hover:border-2 border-blue-800 m-2 !text-white'
+            className='bg-gray-600 hover:bg-black/40 hover:border-2 border-blue-800 m-2'
             onClick={()=>handleOnNavigate(item.slug)}
             >
             {item.title}
